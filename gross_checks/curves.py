a82a9d39b4521c65f98d9bd79fe569d62442a87e
Robert.Grumbine <Robert.Grumbine@noaa.gov>
2021-04-24 13:46:18 +0000

    rename exceptions to regions, continue iteration

--- gross_checks/curves.py
+++ gross_checks/curves.py
@@ -24,10 +24,10 @@ class curve :
       #debug print("unclosed")
       m = self.points[0]
       self.points.append(m)
+    self.npts = len(self.points)
 
   def inside(self, pt):
-    x = inside(pt, self.points)
-    #print("self.inside = ",x)
+    x = inside(pt, self.points, self.npts)
     return(x != 0)
 
 
@@ -42,8 +42,8 @@ def isleft(p0, p1, p2):
   else:
     return 0
 
-def inside(x, curve):
-  npts = len(curve)
+def inside(x, curve, npts):
+  #npts = len(curve)
   #debug print("def inside, x, npts: ",x, x[0], x[1], npts)
 
   unclosed = False
@@ -66,15 +66,15 @@ def inside(x, curve):
           wn -= 1
     #debug print(i,wn)
 
-  if (unclosed):
-    i = npts-1
-    if (curve[i][0] <= x[0]):
-      if (last[0] > x[0]):
-        if (isleft(curve[i], last, x) > 0):
-          wn += 1
-    else:
-      if (last[0] <= x[0]):
-        if (isleft(curve[i], last, x) < 0):
-          wn -= 1
+  #if (unclosed):
+  #  i = npts-1
+  #  if (curve[i][0] <= x[0]):
+  #    if (last[0] > x[0]):
+  #      if (isleft(curve[i], last, x) > 0):
+  #        wn += 1
+  #  else:
+  #    if (last[0] <= x[0]):
+  #      if (isleft(curve[i], last, x) < 0):
+  #        wn -= 1
 
   return wn
---
+++ gross_checks/regions.py
@@ -0,0 +1,36 @@
+# Multiple inheritance from both bounds (value limits) and curves (domains)
+from bounders import *
+from curves import *
+
+class region(curve, bounds) :
+
+  #the line is a character string with the name of the area, the parameter, and its allowed bounds
+  def __init__(self, line):
+    words = line.split()
+    if (len(words) < 6):
+      print("not enough elements in line, can't initialize region class member", line)
+      exit(1)
+ 
+    self.name = words[0]
+    fname = self.name +".curve"
+    # RG: put in curves class
+    # RG: throw exception if can't open file
+    # RG: throw exception if can't read from file (empty or no points)
+    self.read_curve(fname)
+
+    # from class bounds
+    self.set(words[1], words[2], words[3], words[4], words[5])
+
+  def is_ok(self, pt, value, param = "NULL"):
+    tmp = self.inside(pt)         # is the point inside the curve?
+    t2  = self.ptinbounds(value)  # is it out in bounds?
+    if (param != "NULL"):
+      return (tmp and t2) and (param == self.param)    # is it the right parameter?
+    return(tmp and t2)
+
+def any_ok(regions, pt, value, param = "NULL"):
+  any = False
+  for i in range(0, len(regions)):
+    any = any or regions[i].is_ok(pt, value, param)
+  return any
+
