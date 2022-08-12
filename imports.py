f09787bc91d3edecd534f6e4f6e8d16f124c8476
Robert Grumbine <Robert.Grumbine@noaa.gov>
2022-08-12 14:54:23 +0000

    check module imports

---
+++ imports.py
@@ -0,0 +1,31 @@
+import calendar
+import csv
+import datetime
+import glob
+import importlib
+import math
+import matplotlib
+import matplotlib.pyplot as plt
+import netCDF4
+import numpy 
+import numpy as np
+import numpy.ma as ma
+import os
+import pstats
+import scipy.spatial.distance as spsd
+import scipy.stats as sps
+import sys
+import time
+
+from geographiclib.geodesic import Geodesic
+from math import *
+from netCDF4 import Dataset
+from pkgutil import iter_modules
+from sys import version_info
+
+#my modules
+exit(0)
+from struct import *
+from verf_files import *
+import bounders
+from platforms import *
