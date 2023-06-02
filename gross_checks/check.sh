3457306c15de88621a23f1656745a7d3219deca5
Robert.Grumbine <Robert.Grumbine@noaa.gov>
2023-06-02 20:19:32 +0000

    going back to 2018 for rtofs gross checks

--- gross_checks/check.sh
+++ gross_checks/check.sh
@@ -1,15 +1,67 @@
-. $HOME/rgdev/toolbox/misc/python_load.wcoss2
+#!/bin/bash
+# --- Hera
+#SBATCH -J gross
+#SBATCH -e gross.err
+#SBATCH -o gross.out
+#SBATCH -t 5:55:00
+#SBATCH -q batch
+#SBATCH -A marine-cpu
+#SBATCH -N 1
+#SBATCH --mail-type FAIL
+#SBATCH --mail-user robert.grumbine@noaa.gov
+# --- Hera
+
+
+#--- WCOSS2
+##PBS -N rtofs_eval
+##PBS -o rtofs_eval
+##PBS -j oe
+##PBS -A ICE-DEV
+##PBS -q hera
+##PBS -l walltime=1:00:00
+##PBS -l select=1:ncpus=1
+#--- WCOSS2
+
+export MODEL=rtofs_cice
+export modelout=$HOME/clim_data/rtofs_gross
 
 export GDIR=$HOME/rgdev/ice_scoring/gross_checks/
 
-export PYTHONPATH=$PYTHONPATH:$GDIR/shared
-ln -sf $GDIR/curves curves
+#. $HOME/rgdev/toolbox/misc/python_load.wcoss2
+. $HOME/rgdev/toolbox/misc/python_load.hera
 
-#after check has been done
+export PYTHONPATH=$PYTHONPATH:$GDIR/shared
+
+if [ ! -d $HOME/scratch/gross/$MODEL ] ; then
+  mkdir -p  $HOME/scratch/gross/$MODEL
+  if [ $? -ne 0 ] ; then
+    echo zzz could not create rundir  $HOME/scratch/gross/$MODEL
+    exit 1
+  fi
+fi 
+cd  $HOME/scratch/gross/$MODEL
+
+ln -sf $GDIR/curves curves
+$GDIR/$MODEL/${MODEL}_scan.sh
+
+
+#after check has been done -- summaries and graphics
+# beta.$tag.${lead}
+cat beta.*.* > all
+for lead in n00 f24 f48 f72 f96 f120 f144 f168 f192
+do
+  cat beta.*.$lead > all.$lead
+done
+
+
+# For plots, last number is dot size. Expect fewer pts as go down list, 
+#    so make pts larger
 python3 $GDIR/graphics/plot_errs.py all all 1.
 
 python3 $GDIR/exceptions/exceptions.py $GDIR/exceptions/physical.exceptions all > nonphysical
-python3 $GDIR/graphics/plot_errs.py nonphysical nonphysical 1.
+python3 $GDIR/graphics/plot_errs.py nonphysical nonphysical 2.
 
 python3 $GDIR/exceptions/exceptions.py $GDIR/exceptions/known.errors nonphysical > unknown
-python3 $GDIR/graphics/plot_errs.py unknown unknown 1.
+python3 $GDIR/graphics/plot_errs.py unknown unknown 4.
+
+$GDIR/$MODEL/split.sh unknown | sort -n
---
+++ gross_checks/rtofs_cice/rtofs_cice_scan.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+##PBS -N rtofs_eval
+##PBS -o rtofs_eval
+##PBS -j oe
+##PBS -A ICE-DEV
+##PBS -q dev
+##PBS -l walltime=1:00:00
+##PBS -l select=1:ncpus=1
+#Wcoss2
+
+# Run time is about 1 hour per month on rtofs_cice
+
+#set by calling script -----------------------------------------
+#. $HOME/rgdev/toolbox/misc/python_load.wcoss2
+#export GDIR=$HOME/rgdev/ice_scoring/gross_checks
+#cd $GDIR
+
+export PYTHONPATH=$PYTHONPATH:$HOME/rgdev/ice_scoring/gross_checks/shared
+export MODDEF=$HOME/rgdev/ice_scoring/model_definitions
+
+export modelout=${modelout:-$HOME/noscrub/model_intercompare/rtofs_cice}
+
+start=20180607
+end=20180930
+tag=$start
+while [ $tag -le $end ] 
+do
+  mm=`echo $tag | cut -c5-6`
+  dd=`echo $tag | cut -c7-8`
+  for lead in n00 f24 f48 f72 f96 f120 f144 f168 f192
+  do
+    if [ -f $modelout/rtofs.${tag}/rtofs_glo.t00z.${lead}.cice_inst ] ; then
+      time python3 $GDIR/$MODEL/$MODEL.py \
+          $modelout/rtofs.${tag}/rtofs_glo.t00z.${lead}.cice_inst \
+          $GDIR/$MODEL/$MODEL.extremes fly > beta.$tag.${lead}
+      mv fhistogram fhistogram.$tag.$lead
+
+    elif [ -f $modelout/${tag}/rtofs_glo.t00z.${lead}.cice_inst ] ; then
+      time python3 $GDIR/$MODEL/$MODEL.py \
+          $modelout/${tag}/rtofs_glo.t00z.${lead}.cice_inst \
+          $GDIR/$MODEL/$MODEL.extremes fly > beta.$tag.${lead}
+      mv fhistogram fhistogram.$tag.$lead
+    fi
+  done
+
+  tag=`expr $tag + 1`
+  tag=`dtgfix3 $tag`
+done
+
+#  cat beta.${mm}*.* > beta.$mm
+#  python3 $GDIR/plot_errs.py beta.$mm early_$mm
