0673db0a21c342b5eba0483042ed033d96402f8f
Robert.Grumbine <Robert.Grumbine@noaa.gov>
2023-11-08 16:57:24 -0500

    update history refs

--- NCEP_si_verf/ice_edge/vs_nichr/runup.sh
+++ NCEP_si_verf/ice_edge/vs_nichr/runup.sh
@@ -11,16 +11,21 @@ cd $HOME/rgdev/edges/
 
 . $HOME/rgdev/ice_scoring/NCEP_si_verf/ice_edge/vs_nichr/bootstrap.sh
 
+# For persistence scoring, NIC vs. NIC
 ./nicedge_parse.sh
 
 for dy in 1 2 3 4 5 6 7 8 10 16 365
 do
   echo working on $dy day lead
-
   time python3 dy_score.py 20230101 $dy 
-
 done
+./nic_plot_this_year.sh
 
+# For rtofs scoring (needs nicedge_parse.sh to have run, bt doesn't need 
+#   the others between there and here
+# Find rtofs edges and score them
 time python3 rtofs.py
+# Produce graphics (relies on rtofs_edge_rmse.py)
+echo zzz make rtofs score graphics
+time ./rtofs_score.sh
 
-./this_year.sh
