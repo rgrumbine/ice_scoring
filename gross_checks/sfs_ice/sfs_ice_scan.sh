294ac500233b6755983623ad3989997cb4d1c95d
Robert.Grumbine <Robert.Grumbine@noaa.gov>
2026-02-20 16:34:36 +0000

    checking sfsbeta

--- gross_checks/sfs_ice/sfs_ice_scan.sh
+++ gross_checks/sfs_ice/sfs_ice_scan.sh
@@ -1,11 +1,11 @@
 #!/bin/sh 
 
 # ursa, WCOSS2
-#export PYTHONPATH=$HOME/rgdev/mmablib/py:$HOME/rgdev/ice_scoring/gross_checks/
-#export MODDEF=$HOME/rgdev/ice_scoring/model_definitions
+export PYTHONPATH=$HOME/rgdev/mmablib/py:$HOME/rgdev/ice_scoring/gross_checks/
+export MODDEF=$HOME/rgdev/ice_scoring/model_definitions
 # gaea c6
-export PYTHONPATH=$HOME/rg6/mmablib/py:$HOME/rgdev/ice_scoring/gross_checks/
-export MODDEF=$HOME/rg6/ice_scoring/model_definitions
+#export PYTHONPATH=$HOME/rg6/mmablib/py:$HOME/rgdev/ice_scoring/gross_checks/
+#export MODDEF=$HOME/rg6/ice_scoring/model_definitions
 
 echo zzz module list
 module list
@@ -15,11 +15,7 @@ set -x
 export level=extreme
 export cyc=00
 
-tag=20231101
-#yy=`echo $tag | cut -c1-4`
-#mm=`echo $tag | cut -c5-6`
-#dd=`echo $tag | cut -c7-8`
-#j=0
+tag=20130301
 
 if [ -d $modelout ] ; then
   #for fhr in 006 012 
@@ -27,10 +23,10 @@ if [ -d $modelout ] ; then
   while [ $fhr -le 8784 ]
   do
     time python3 $GDIR/universal2d.py \
-           $modelout/sfs.t${cyc}z.tripolar.f${fhr}.nc \
+           $modelout/sfs.$tag/00/mem000/products/ice/netcdf/native/sfs.t${cyc}z.native.f${fhr}.nc \
            cice.header \
            $GDIR/ctl/sfs.20231101 redone \
-           > gfs.cice.$level.$fhr.results
+           > sfs.cice.$level.$fhr.results
     fhr=`expr $fhr + 24`
     if [ $fhr -le 100 ] ; then
       fhr=0$fhr
@@ -38,5 +34,5 @@ if [ -d $modelout ] ; then
   done
 fi
 
-cat gfs.cice.*.results > all
+cat sfs.cice.*.results > all
 
